7eadc10e185d420e4cbb8d44819f03d1eacaa1bf
Mark Furland <mark@fur.land>
2022-06-23 18:10:09 -0400

    logging change that was left uncommitted

--- csv_logger.py
+++ csv_logger.py
@@ -4,25 +4,29 @@ from csv import DictWriter
 from datetime import datetime
 import json
 from os.path import isfile
+import logging
 
 fieldnames = ("time","isKnown","access","username","uid","door")
 
 def on_connect(client, userdata, flags, rc):
-    print(f'Connected with result code {rc}')
+    logger = logging.getLogger('on_connect')
+    logger.info(f'Connected with result code {rc}')
 
     global config
     for section in config.sections():
         if section.endswith('door'):
             topic = f"{config[section]['topic']}/send"
-            print(f'subscribed to topic {topic}')
+            logger.info(f'subscribed to topic {topic}')
             client.subscribe(topic)
 
 def on_message(client, userdata, msg):
+    logger = logging.getLogger('on_message')
     data = json.loads(msg.payload)
+    logger.debug(f'message: {data}')
     if data['cmd'] == 'log' and data['type'] == 'access':
         data['time'] = datetime.fromtimestamp(data['time'])
         data['uid'] = int_to_uid(int(data['uid']))
-        print(f"{data['time']}: {data['username']} ({data['uid']}, {data['access']}) at {data['door']}")
+        logger.info(f"{data['time']}: {data['username']} ({data['uid']}, {data['access']}) at {data['door']}")
         with open('access.csv','a',newline='') as csvfile:
             log = DictWriter(csvfile,fieldnames=fieldnames,extrasaction='ignore')
             log.writerow(data)
@@ -33,6 +37,8 @@ def int_to_uid(uid_int):
     return f'{upper:05d}:{lower:05d}'
 
 def main():
+    logging.basicConfig(level=logging.DEBUG,format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s\t{%(filename)s:%(funcName)s:%(lineno)d}')
+    logger = logging.getLogger('main')
     if not isfile('access.csv'):
         with open('access.csv','a',newline='') as csvfile:
             log = DictWriter(csvfile,fieldnames=fieldnames,extrasaction='ignore')
@@ -46,7 +52,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    print(f"connecting to {config['mqtt']['server']}:{config.getint('mqtt','port')}")
+    logger.info(f"connecting to {config['mqtt']['server']}:{config.getint('mqtt','port')}")
     client.connect(config['mqtt']['server'],config.getint('mqtt','port'))
 
     client.loop_forever()
