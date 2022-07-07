23c3ed918d21bf6ca24e3e864e5ced7821a952a6
Mark Furland <mark@fur.land>
2022-07-07 00:28:10 -0400

    put in a bunch of delays and it mostly works sometimes now

--- setAccess.js
+++ setAccess.js
@@ -5,17 +5,37 @@ const rp = require('request-promise-native');
 const csv=require('csvtojson')
 const fs = require('fs');
 
+const args = process.argv.slice(2);
+if(args.length != 1 || args[0] == "-h" || args[0] == "--help") {
+    console.log("usage: setAccess.js <access.csv>");
+    console.log("   update doors configured in config.json with people in access.csv");
+    console.log("   access.csv header: CID, name, key serial, PIN");
+    process.exit(1);
+}
+let fileToParse = args[0];
+
+try {
+    if (! fs.existsSync(fileToParse)) {
+        console.error(`no such file: "${fileToParse}"`);
+        process.exit(2);
+    }
+} catch(err) {
+    console.error(`error opening file: "${fileToParse}"`);
+    console.error(err)
+    process.exit(2);
+}
+
 let config = JSON.parse(fs.readFileSync('config.json'));
 
-// parse csv of "cid, Name, Serial, PIN"
+// parse csv of "CID, name, key serial, PIN"
 const getExpectedUsers = (filename) => {
     return csv()
         .fromFile(filename)
         .then((users) => users.map((user) => {
             return {
-                uid: user.Serial,
+                uid: user['key serial'],
                 acctype: 1,
-                username: `${user.cid}: ${user.Name}`,
+                username: `${user.CID}: ${user.name}`,
                 validuntil: 4200000000, //year 2103, probably fine
                 pincode: `${user.PIN}`
             };
@@ -42,11 +62,6 @@ const login = (host, username, password) => {
     })
 }
 
-
-//getExpectedUsers('users.csv').then((users) => {
-//  console.log(users);
-//});
-
 const userTypes = {
     Always: 1,
     Disabled: 0,
@@ -56,13 +71,14 @@ const userTypes = {
 const getActualUsers = (ws) => {
     return new Promise((resolve, reject) => {
         let users = [];
-        ws.on('message', (message) => {
+        ws.on('message', async (message) => {
             let data = JSON.parse(message)
             //console.log(data);
             if(data.command == 'userlist') {
                 users = users.concat(data.list);
                 console.log(`parsed userlist page ${data.page} of ${data.haspages}`);
                 if(data.page < data.haspages) {
+                    await delay(3000);
                     ws.send('{"command":"userlist", "page":' + (data.page + 1) + '}');
                 } else {
                     resolve(users);
@@ -95,27 +111,39 @@ const connect = (auth, ip) => {
         });
     });
 };
-const deleteUsers = (ws, badUsers, ip) => {
-    badUsers.forEach((u) => {
-        ws.send(JSON.stringify( {
-            "command": "remove",
-            "uid": u.uid
-        }));
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const deleteUsers = async (ws, badUsers) => {
+    return badUsers.map((u, i) => {
+        return new Promise(async (resolve, reject) => {
+            await delay(i * 1500);
+            ws.send(JSON.stringify( {
+                "command": "remove",
+                "uid": u.uid
+            }));
+            resolve();
+        });
     });
 }
-const addUsers = (ws, users, ip) => {
-    users.forEach((u) => {
-        ws.send(JSON.stringify( {
-            "command":"userfile",
-            "uid":u.uid,
-            "pincode":u.pincode,
-            "user":u.username,
-            "acctype":1,
-            "acctype2":null,
-            "acctype3":null,
-            "acctype4":null,
-            "validuntil":u.validuntil
-        }));
+
+const addUsers = (ws, users) => {
+    return users.map((u, i) => {
+        return new Promise(async (resolve, reject) => {
+            await delay(i * 1500);
+            ws.send(JSON.stringify( {
+                "command":"userfile",
+                "uid":u.uid,
+                "pincode":u.pincode,
+                "user":u.username,
+                "acctype":1,
+                "acctype2":null,
+                "acctype3":null,
+                "acctype4":null,
+                "validuntil":u.validuntil
+            }));
+            resolve();
+        });
     });
 }
 
@@ -156,22 +184,27 @@ const keypress = async () => {
                 return connect(auth, host.ip)
             }) .then(async (ws) => {
                 console.log('connected to websocket');
+                await delay(1000);
                 const actualUsers = await getActualUsers(ws);
-                const expectedUsers = await getExpectedUsers('users.csv');
+                const expectedUsers = await getExpectedUsers(fileToParse);
 
                 const badUsers = onlyInLeft(actualUsers, expectedUsers, isSameUser);
                 const missingUsers = onlyInLeft(expectedUsers, actualUsers, isSameUser);
                 // I guess it won't let us add duplicate UIDs
                 //const duplicateUsers = duplicates( actualUsers, isSameUser);
-                console.log("bad users");
-                console.log(badUsers);
-                console.log("missing users");
-                console.log(missingUsers);
+                //console.log("bad users");
+                //console.log(badUsers);
+                //console.log("missing users");
+                //console.log(missingUsers);
                 //console.log("duplicate users");
                 //console.log(duplicateUsers);
 
-                deleteUsers(ws, badUsers);
-                addUsers(ws, missingUsers)
+                console.log(`deleting ${badUsers.length} users`);
+                console.log(`adding ${missingUsers.length} users`);
+                await deleteUsers(ws, badUsers);
+                console.log("done removing");
+                await Promise.all(addUsers(ws, missingUsers));
+                console.log("done adding");
                 process.exit(0);
             });
     });
